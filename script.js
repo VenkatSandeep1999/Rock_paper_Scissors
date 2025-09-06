247b9b2b165ca95d737198a1b66d4a945788c831
VenkatSandeep1999 <vgadi4@unh.newhaven.edu>
2025-09-06 16:47:45 -0400

    This commit consists of all the changes in the code. This is a complete working code

--- script.js
+++ script.js
@@ -11,14 +11,37 @@ function getComputerChoice(){
     }
 }
 function getHumanChoice(){
-    let decision=prompt("Enter your decision")
+    let decision=prompt("Enter your decision: (rock/paper/scissors)")
     return decision.toLowerCase()
 }
 
-let comvalue= getComputerChoice();
-console.log(`Computer value is ${comvalue}`)
-let hvalue = getHumanChoice();
-console.log(`Human value is ${hvalue}`)
-
-let humanScore=0,computerScore=0;
-
+function playRound(humanChoice,computerChoice){
+    if((humanChoice=='rock' && computerChoice=='paper')||(humanChoice=='paper' && computerChoice=='scissors') || (humanChoice=='scissors' && computerChoice=='rock')){
+        console.log(`You lose! ${computerChoice} wins ${humanChoice}`);
+        computerScore++;
+    }
+    else if(humanChoice==computerChoice){
+        console.log("Draw");
+    }
+    else{
+        console.log(`You Win! ${humanChoice} wins ${computerChoice}`)
+        humanScore++;
+    }
+    console.log(`Human Score: ${humanScore} || Computer Score: ${computerScore}`)
+}
+function playGame(){
+    for(let i=0;i<5;i++){
+        humanChoice = getHumanChoice();
+        computerChoice= getComputerChoice();
+        console.log(`Computer value is ${computerChoice}`)
+        console.log(`Human value is ${humanChoice}`)
+        playRound(humanChoice,computerChoice)
+    }
+    if(humanScore>computerScore)
+        console.log("Hurrayy!! You won")
+    else
+        console.log("Alas!! You lost")
+}
+let humanScore=0;
+let computerScore=0;
+playGame()
